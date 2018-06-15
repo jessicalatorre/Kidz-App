4686304e1bc138ef3d3a3e0ada1b086ade9d269e
alctrs <mgamiddo@gmail.com>
2018-06-14 19:39:17 -0500

    config_models sessions.js and seeders

---
+++ migrations/20180614231941-create-sessions.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('sessions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userID: {
+        type: Sequelize.STRING
+      },
+      objectName: {
+        type: Sequelize.STRING
+      },
+      isCorrect: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('sessions');
+  }
+};
