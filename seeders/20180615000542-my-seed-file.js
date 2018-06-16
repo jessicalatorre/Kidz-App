0234def563660fd81e68233249abb2af7472c06b
Jessica La Torre <jess.latorre@gmail.com>
2018-06-16 13:17:19 -0500

    Created parent.app.js file, parent-view.html, and parent-mode-routes.js files.

--- seeders/20180615000542-my-seed-file.js
+++ seeders/20180615000542-my-seed-file.js
@@ -11,6 +11,50 @@ module.exports = {
       
     }], {});
   },
+
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('sessions', [{
+      UserID : 1,
+      ObjectName : 'Duck',
+      IsCorrect : true,
+      createdAt : new Date(),
+      updatedAt : new Date(),
+      
+    }], {});
+  },
+
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('sessions', [{
+      UserID : 1,
+      ObjectName : 'Hose',
+      IsCorrect : true,
+      createdAt : new Date(),
+      updatedAt : new Date(),
+      
+    }], {});
+  },
+
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('sessions', [{
+      UserID : 1,
+      ObjectName : 'Goat',
+      IsCorrect : true,
+      createdAt : new Date(),
+      updatedAt : new Date(),
+      
+    }], {});
+  },
+
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('sessions', [{
+      UserID : 1,
+      ObjectName : 'Goat',
+      IsCorrect : true,
+      createdAt : new Date(),
+      updatedAt : new Date(),
+      
+    }], {});
+  },
 }
   
   
--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 // Server.js - This file is the initial starting point for the Node/Express server.
 //
 // ******************************************************************************
@@ -7,6 +6,9 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require('path');
+var passport = require('passport');
+var session    = require('express-session')
+//var env        = require('dotenv').load()
 
 // Sets up the Express App
 // =============================================================
@@ -14,7 +16,7 @@ var app = express();
 var PORT = process.env.PORT || 8080;
 
 // Requiring our models for syncing
-// var db = require("./models");
+var db = require("./models");
 
 // Sets up the Express app to handle data parsing
 
@@ -28,6 +30,7 @@ app.use(session({ secret: 'keyboard cat',resave: true, saveUninitialized:true}))
 app.use(passport.initialize());
 
 app.use(passport.session()); // persistent login sessions
+require('./config/passport/passport.js')(passport, db.user);
 
 // Static directory
 // app.use(express.static("public"));
@@ -37,27 +40,26 @@ app.use(express.static("public"));
 // Routes
 require("./routes/html-routes.js")(app);
 require("./routes/API_Routes.js")(app);
+require("./routes/passport-routes")(app,passport);
+
 
 //Below is the test to ensure all the models are imported correctly via the server.js file
 
-//Models
-var models = require("./models");
- 
 //Sync Database
-models.sequelize.sync().then(function() {
- 
-    console.log('HUZZAH! Database looks fine')
- 
+db.sequelize.sync().then(function() {
+
+   console.log('HUZZAH! Database looks fine')
+
 }).catch(function(err) {
- 
-    console.log(err, "Something went wrong with the Database Update!")
- 
+
+   console.log(err, "Something went wrong with the Database Update!")
+
 });
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
 // db.sequelize.sync({ force: true }).then(function() {
-  app.listen(PORT, function() {
-    console.log("App listening on PORT http://localhost:" + PORT);
-  });
+ app.listen(PORT, function() {
+   console.log("App listening on PORT http://localhost:" + PORT);
+ });
 // });
