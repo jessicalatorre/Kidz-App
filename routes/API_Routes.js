2dfdb1e0e0d730a6d73d32f6888e4e4f411751b3
Jessica La Torre <jess.latorre@gmail.com>
2018-06-15 21:26:42 -0500

    Created user.js model. Next step is to create migration file and seed data file to migrate data to MySql database.

--- routes/API_Routes.js
+++ routes/API_Routes.js
@@ -1,5 +1,4 @@
 
-// GET on Session Data from MySql Using Sequelize
 var express = require("express");
 
 var path = require('path')
@@ -7,6 +6,7 @@ var app = express();
 
 var db = require("../models");
 
+// GET on Session Data from MySql Using Sequelize
 module.exports = function(app) {
     app.get("/api/sessions", function(req, res) {
       // Here we add an "include" property to our options in our findAll query
@@ -19,7 +19,13 @@ module.exports = function(app) {
       });
     });
 
-//NEXT! Create method to record sessions data (clicks) - POST   dbSessions.create
+//POST method to record sessions data (clicks) 
+app.post("/api/sessions", function(req, res) {
+  db.sessions.create(req.body).then(function(dbsessions) {
+    res.json(dbsessions);
+  });
+});
+
 
 };
 
